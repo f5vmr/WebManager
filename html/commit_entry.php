b274d454517afaa13688a312ba59268464cb8183
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-22 21:01:34 +0000

    update debugging

--- html/commit_entry.php
+++ html/commit_entry.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 if (!isset($_SESSION['admin_logged_in'])) {
     // For standalone dev mode, you can comment this line:
