10f3d456ff1ac956a23c509e3f359ce07785d63e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-22 14:58:43 +0000

    update

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
 // Keys are callsigns (usernames), values are password hashes
 // Generate a hash with password_hash('yourpassword', PASSWORD_DEFAULT)
 $ADMIN_USERS = [
-    "G4NAB" => '$2y$10$QgsEcIxoKjGa0h1nAv06LuF8ewHCFaPYTn/2jbPaWcYG/c.GsYHS6'
+    "G4NAB" => '$2y$10$QgsEcIxoKjGa0h1nAv06LuF8ewHCFaPYTn/2jbPaWcYG/c.GsYHS6',
     "M0YDG" => '$2y$10$E6p1JdW5l9J5kQDlP/0kOuN9VhXxuhPj.k2zI.xYeVx/7xYhM9sI2',
     "M0DIT" => '$2y$10$7pVx7/4GqTnZK0D1a3j9JeIUKnTkl2F1hY9wLJQdT8G9GgFHoHxOy'
 ];
