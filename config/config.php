f9895f73de48b92c39b5cf25ad8c19e0cefacd75
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2025-11-22 14:52:50 +0000

    update

--- config/config.php
+++ config/config.php
@@ -5,8 +5,9 @@
 // Keys are callsigns (usernames), values are password hashes
 // Generate a hash with password_hash('yourpassword', PASSWORD_DEFAULT)
 $ADMIN_USERS = [
-    "M0ABC" => '$2y$10$E6p1JdW5l9J5kQDlP/0kOuN9VhXxuhPj.k2zI.xYeVx/7xYhM9sI2',
-    "G7XYZ" => '$2y$10$7pVx7/4GqTnZK0D1a3j9JeIUKnTkl2F1hY9wLJQdT8G9GgFHoHxOy'
+    "G4NAB" => '$2y$10$QgsEcIxoKjGa0h1nAv06LuF8ewHCFaPYTn/2jbPaWcYG/c.GsYHS6'
+    "M0YDG" => '$2y$10$E6p1JdW5l9J5kQDlP/0kOuN9VhXxuhPj.k2zI.xYeVx/7xYhM9sI2',
+    "M0DIT" => '$2y$10$7pVx7/4GqTnZK0D1a3j9JeIUKnTkl2F1hY9wLJQdT8G9GgFHoHxOy'
 ];
 
 // ============================
@@ -18,3 +19,7 @@ function logAdminAction($username, $action) {
     $entry = sprintf("[%s] - %s - %s - %s\n", date('Y-m-d H:i:s'), $ip, $username, $action);
     file_put_contents($logFile, $entry, FILE_APPEND);
 }
+
+
+
+
